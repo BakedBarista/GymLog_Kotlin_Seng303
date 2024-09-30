108cd1887a081e7e028d34020ad67c7087a3a2c9
Ryan Scofield <rsc104@uclive.ac.nz>
2024-10-01 01:42:23 +1300

    FEAT: Made bottom navbar with icons - onclicks not implemented yet - need to setup navigation first

--- app/src/main/java/com/example/seng303_groupb_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/seng303_groupb_assignment2/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.seng303_groupb_assignment2.ui.theme.SENG303_GroupB_Assignment2Theme
 
@@ -19,11 +27,12 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SENG303_GroupB_Assignment2Theme {
-                Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    bottomBar = { CustomBottomAppBar() }
+                ) { paddingValues ->
+                    Box(modifier = Modifier.fillMaxSize().padding(paddingValues)) {
+                        // TODO
+                    }
                 }
             }
         }
@@ -31,17 +40,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SENG303_GroupB_Assignment2Theme {
-        Greeting("Android")
+fun CustomBottomAppBar() {
+    BottomAppBar {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.run),
+                    contentDescription = "Icon 1"
+                )
+            }
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.add),
+                    contentDescription = "Icon 2"
+                )
+            }
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.progress),
+                    contentDescription = "Icon 3"
+                )
+            }
+            IconButton(onClick = { /* TODO */ }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.leaderboard),
+                    contentDescription = "Icon 4"
+                )
+            }
+        }
     }
 }
