75f6f13f43d86ec0a56e784af01e8cb1471f3e8b
Ryan Scofield <rsc104@uclive.ac.nz>
2024-10-02 00:21:26 +1300

    FEAT: Setup basic top app bar to display the title of the page that the user is on

--- app/src/main/java/com/example/seng303_groupb_assignment2/MainActivity.kt
+++ app/src/main/java/com/example/seng303_groupb_assignment2/MainActivity.kt
@@ -10,10 +10,17 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
@@ -34,24 +41,34 @@ class MainActivity : ComponentActivity() {
         setContent {
             SENG303_GroupB_Assignment2Theme {
                 val navController = rememberNavController()
+
+                // TODO - make this use string resources instead of hard coded string literals
+                var currentTitle by rememberSaveable { mutableStateOf("Home") }
+
                 Scaffold(
+                    topBar = { CustomTopAppBar(title = currentTitle) },
                     bottomBar = { CustomBottomAppBar(navController) }
                 ) { padding ->
                     Box(modifier = Modifier.padding(padding)) {
                         NavHost(navController = navController, startDestination = "Home") {
                             composable("Home") {
+                                currentTitle = "Home"
                                 Home(navController = navController)
                             }
                             composable("Run") {
+                                currentTitle = "Run Workout"
                                 RunWorkout(navController = navController)
                             }
                             composable("Add") {
+                                currentTitle = "Workout Builder"
                                 AddWorkout(navController = navController)
                             }
                             composable("Progress") {
+                                currentTitle = "View Progress"
                                 ViewProgress(navController = navController)
                             }
                             composable("Leaderboard") {
+                                currentTitle = "Leaderboard"
                                 ViewLeaderboard(navController = navController)
                             }
                         }
@@ -103,4 +120,19 @@ fun CustomBottomAppBar(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomTopAppBar(
+    title: String
+) {
+    // TODO possibly update this to center the text and maybe make the text larger.
+    TopAppBar(
+        title = {
+            Text(
+                text = title,
+            )
+        }
+    )
 }
