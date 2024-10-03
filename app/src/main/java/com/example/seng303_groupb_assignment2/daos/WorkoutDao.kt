a82a085d5943f151bac1103bdedc09c3f65f89bc
Ryan Scofield <rsc104@uclive.ac.nz>
2024-10-03 20:25:12 +1300

    FEAT: Updated WorkoutDao to have functionality for  getting all workouts

--- app/src/main/java/com/example/seng303_groupb_assignment2/daos/WorkoutDao.kt
+++ app/src/main/java/com/example/seng303_groupb_assignment2/daos/WorkoutDao.kt
@@ -7,6 +7,7 @@ import androidx.room.Upsert
 import com.example.seng303_groupb_assignment2.entities.Workout
 import com.example.seng303_groupb_assignment2.entities.WorkoutExerciseCrossRef
 import com.example.seng303_groupb_assignment2.entities.WorkoutWithExercises
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface WorkoutDao {
@@ -15,6 +16,10 @@ interface WorkoutDao {
 
     @Upsert
     suspend fun upsertWorkoutExerciseCrossRef(crossRef: WorkoutExerciseCrossRef)
+
+    @Transaction
+    @Query("SELECT * FROM Workout ORDER BY name ASC")
+    fun getAllWorkouts(): Flow<List<Workout>>
 
     @Transaction
     @Query("SELECT * FROM Workout WHERE id = :workoutId")
