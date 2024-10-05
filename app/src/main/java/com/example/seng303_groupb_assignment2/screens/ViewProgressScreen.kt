c0c30d767e150e24e2cf11f168505fb8cb027e8e
Ben Dawson <bda42@uclive.ac.nz>
2024-10-05 15:41:19 +1300

    Feat: More styling, split screen into separate composables

--- app/src/main/java/com/example/seng303_groupb_assignment2/screens/ViewProgressScreen.kt
+++ app/src/main/java/com/example/seng303_groupb_assignment2/screens/ViewProgressScreen.kt
@@ -3,7 +3,6 @@ package com.example.seng303_groupb_assignment2.screens
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -26,143 +25,220 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.navigation.NavController
+import com.example.seng303_groupb_assignment2.R
 import com.example.seng303_groupb_assignment2.entities.Exercise
+import com.example.seng303_groupb_assignment2.entities.ExerciseLog
+import com.example.seng303_groupb_assignment2.graphcomponents.CircleComponent
 import com.example.seng303_groupb_assignment2.utils.exerciseSaver
 import com.example.seng303_groupb_assignment2.viewmodels.ExerciseViewModel
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberBottom
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberStart
+import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLine
 import com.patrykandpatrick.vico.compose.cartesian.layer.rememberLineCartesianLayer
 import com.patrykandpatrick.vico.compose.cartesian.marker.rememberDefaultCartesianMarker
 import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
+import com.patrykandpatrick.vico.compose.cartesian.rememberVicoScrollState
 import com.patrykandpatrick.vico.compose.common.component.rememberTextComponent
+import com.patrykandpatrick.vico.compose.common.fill
+import com.patrykandpatrick.vico.core.cartesian.Scroll
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianChartModelProducer
+import com.patrykandpatrick.vico.core.cartesian.data.CartesianLayerRangeProvider
 import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
+import com.patrykandpatrick.vico.core.cartesian.layer.LineCartesianLayer
+import com.patrykandpatrick.vico.core.common.component.Component
+import com.patrykandpatrick.vico.core.common.data.ExtraStore
 import org.koin.androidx.compose.getViewModel
+import kotlin.math.ceil
 
 @Composable
 fun ViewProgress(
     navController: NavController,
     viewModel: ExerciseViewModel = getViewModel()
 ) {
+    // TODO Remove this when we don't need sample data anymore
     viewModel.createSampleExerciseAndLogs()
     var selectedExercise by rememberSaveable(stateSaver = exerciseSaver) { mutableStateOf<Exercise?>(null) }
     var showDialog by rememberSaveable { mutableStateOf(false) }
     var searchQuery by rememberSaveable { mutableStateOf("") }
+    var selectedOption by rememberSaveable { mutableStateOf(ChartOption.MaxMeasurement2) }
+
     val exercises by viewModel.getExercisesByName(searchQuery).observeAsState(emptyList())
     val exerciseLogs by viewModel.getExerciseLogsByExercise(selectedExercise?.id ?: 0L)
         .observeAsState(emptyList())
 
+    Column(modifier = Modifier.padding(16.dp)) {
+
+        ExerciseHeader(selectedExercise) {
+            showDialog = true
+        }
+
+        if (showDialog) {
+            ExerciseSelectionDialog(
+                exercises = exercises,
+                searchQuery = searchQuery,
+                onSearchQueryChanged = { searchQuery = it },
+                onExerciseSelected = {
+                    selectedExercise = it
+                    showDialog = false
+                },
+                onDismissRequest = { showDialog = false }
+            )
+        }
+
+        if (selectedExercise != null && exerciseLogs.isNotEmpty()) {
+            ExerciseProgressGraph(exerciseLogs)
+        }
+    }
+}
+
+enum class ChartOption(val label: String) {
+    MaxMeasurement2("Max Measurement2"),
+    TotalWorkoutVolume("Total Workout Volume")
+}
+
+@Composable
+fun ExerciseHeader(
+    selectedExercise: Exercise?,
+    onHeaderClick: () -> Unit
+) {
+    Text(
+        text = selectedExercise?.name ?: stringResource(R.string.tap_select),
+        style = MaterialTheme.typography.headlineMedium,
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onHeaderClick() }
+            .padding(16.dp),
+        color = MaterialTheme.colorScheme.primary
+    )
+}
+
+@Composable
+fun ExerciseSelectionDialog(
+    exercises: List<Exercise>,
+    searchQuery: String,
+    onSearchQueryChanged: (String) -> Unit,
+    onExerciseSelected: (Exercise) -> Unit,
+    onDismissRequest: () -> Unit
+) {
     val focusRequester = remember { FocusRequester() }
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
 
-    Column(modifier = Modifier.padding(16.dp)) {
-
-        Text(
-            text = selectedExercise?.name ?: "Tap to select an exercise",
-            style = MaterialTheme.typography.headlineMedium,
+    Dialog(onDismissRequest = onDismissRequest) {
+        Surface(
             modifier = Modifier
                 .fillMaxWidth()
-                .clickable { showDialog = true }
                 .padding(16.dp),
-            color = MaterialTheme.colorScheme.primary
-        )
+            shape = MaterialTheme.shapes.medium,
+            tonalElevation = 8.dp
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+                DisposableEffect(Unit) {
+                    focusRequester.requestFocus()
+                    keyboardController?.show()
+                    onDispose { }
+                }
 
-        if (showDialog) {
-            Dialog(onDismissRequest = { showDialog = false }) {
-                Surface(
+                TextField(
+                    value = searchQuery,
+                    onValueChange = onSearchQueryChanged,
+                    label = { Text(stringResource(R.string.search_exercise)) },
                     modifier = Modifier
                         .fillMaxWidth()
-                        .padding(16.dp),
-                    shape = MaterialTheme.shapes.medium,
-                    tonalElevation = 8.dp
-                ) {
-                    Column(modifier = Modifier.padding(16.dp)) {
-                        DisposableEffect(Unit) {
-                            focusRequester.requestFocus()
-                            keyboardController?.show()
-                            onDispose { }
+                        .focusRequester(focusRequester)
+                )
+
+                Spacer(modifier = Modifier.height(8.dp))
+
+                LazyColumn {
+                    if (exercises.isEmpty()) {
+                        item {
+                            Text(
+                                text = stringResource(R.string.no_exercises),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(8.dp),
+                                style = MaterialTheme.typography.bodyMedium,
+                                color = MaterialTheme.colorScheme.onSurface
+                            )
                         }
-                        TextField(
-                            value = searchQuery,
-                            onValueChange = { searchQuery = it },
-                            label = { Text("Search for an exercise") },
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .focusRequester(focusRequester)
-                        )
-
-                        Spacer(modifier = Modifier.height(8.dp))
-
-                        LazyColumn {
-                            if (exercises.isEmpty()) {
-                                item {
-                                    Text(
-                                        text = "No exercises found.",
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .padding(8.dp),
-                                        style = MaterialTheme.typography.bodyMedium,
-                                        color = MaterialTheme.colorScheme.onSurface
-                                    )
-                                }
-                            } else {
-                                items(exercises) { exercise ->
-                                    Text(
-                                        text = exercise.name,
-                                        modifier = Modifier
-                                            .fillMaxWidth()
-                                            .clickable {
-                                                selectedExercise = exercise
-                                                showDialog = false
-                                                focusManager.clearFocus()
-                                            }
-                                            .padding(8.dp)
-                                    )
-                                }
-                            }
+                    } else {
+                        items(exercises) { exercise ->
+                            Text(
+                                text = exercise.name,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        onExerciseSelected(exercise)
+                                        focusManager.clearFocus()
+                                    }
+                                    .padding(8.dp)
+                            )
                         }
                     }
                 }
             }
-        }
-
-        if (selectedExercise != null && exerciseLogs.isNotEmpty()) {
-            val modelProducer = remember { CartesianChartModelProducer() }
-            LaunchedEffect(exerciseLogs) {
-                val dataSeries = exerciseLogs.map { log ->
-                    log.measurement2.values.maxOrNull() ?: 0f
-                }
-
-                modelProducer.runTransaction {
-                    lineSeries {
-                        series(dataSeries)
-                    }
-                }
-            }
-
-            CartesianChartHost(
-                chart = rememberCartesianChart(
-                    rememberLineCartesianLayer(),
-                    startAxis = VerticalAxis.rememberStart(),
-                    bottomAxis = HorizontalAxis.rememberBottom(),
-                    marker = rememberDefaultCartesianMarker(
-                        label = rememberTextComponent()
-                    )
-                ),
-                modelProducer = modelProducer,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 16.dp),
-            )
         }
     }
 }
+
+@Composable
+fun ExerciseProgressGraph(exerciseLogs: List<ExerciseLog>) {
+    val modelProducer = remember { CartesianChartModelProducer() }
+    LaunchedEffect(exerciseLogs) {
+        val dataSeries = exerciseLogs.map { log ->
+            log.measurement2.values.maxOrNull() ?: 0f
+        }
+
+        modelProducer.runTransaction {
+            lineSeries {
+                series(dataSeries)
+            }
+        }
+    }
+
+    CartesianChartHost(
+        chart = rememberCartesianChart(
+            rememberLineCartesianLayer(
+                LineCartesianLayer.LineProvider.series(
+                    LineCartesianLayer.rememberLine(
+                        fill = remember { LineCartesianLayer.LineFill.single(fill(Color.DarkGray)) },
+                        pointConnector = remember { LineCartesianLayer.PointConnector.cubic(curvature = 0f) },
+                    )
+                ),
+                rangeProvider = rangeProvider
+            ),
+            startAxis = VerticalAxis.rememberStart(),
+            bottomAxis = HorizontalAxis.rememberBottom(),
+            marker = rememberDefaultCartesianMarker(
+                label = rememberTextComponent(),
+                indicator = pointIndicator
+            )
+        ),
+        modelProducer = modelProducer,
+        scrollState = rememberVicoScrollState(true, Scroll.Absolute.End),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 16.dp),
+    )
+}
+
+val pointIndicator: (Color) -> Component = { color ->
+    CircleComponent(colour = color, radius = 10f) // Adjust the radius as needed
+}
+
+// This is how we set the Y axis > than the max val of the Y in out dataset
+private val rangeProvider =
+    object : CartesianLayerRangeProvider {
+        override fun getMaxY(minY: Double, maxY: Double, extraStore: ExtraStore) = ceil(1.1 * maxY)
+    }
